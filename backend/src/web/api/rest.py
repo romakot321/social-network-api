1a73c88c1f779d800c5ffcb62013b9404ca194b0
romakot321 <play62342@gmail.com>
2025-07-29 15:32:29 +0300

    Add dates links to index

--- backend/src/web/api/rest.py
+++ backend/src/web/api/rest.py
@@ -26,19 +26,55 @@ templates = Jinja2Templates(directory="templates")
 
 
 @router.get("/", response_class=HTMLResponse)
-async def creators_list_page(request: Request, account_uow: AccountUoWDepend, task_uow: TaskUoWDepend, params: AccountListParamsDTO = Depends()):
+async def creators_list_page(
+    request: Request, account_uow: AccountUoWDepend, task_uow: TaskUoWDepend, params: AccountListParamsDTO = Depends()
+):
     fotobudka_client = FotobudkaClient(AsyncHttpClient())
     creators = await GetCreatorsListUseCase(task_uow, account_uow, fotobudka_client).execute(params)
-    return templates.TemplateResponse("index.html", {"request": request, "creators": creators, "params": params})
+    today = datetime.datetime.now().replace(hour=0, minute=0, second=0)
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "creators": creators,
+            "params": params,
+            "dates": {
+                "today_start": today,
+                "today_end": today.replace(hour=23, minute=59, second=59),
+                "yesterday_start": today - datetime.timedelta(days=1),
+                "yesterday_end": (today - datetime.timedelta(days=1)).replace(hour=23, minute=59, second=59),
+                "current_month_start": today.replace(day=1),
+                "current_month_end": ((today.replace(day=1, month=today.month + 1)) - datetime.timedelta(days=1)).replace(hour=23, minute=59, second=59),
+                "previous_month_start": today.replace(day=1, month=today.month - 1),
+                "previous_month_end": (today.replace(day=1) - datetime.timedelta(days=1)).replace(hour=23, minute=59, second=59),
+                "all_time_end": today.replace(hour=23, minute=59, second=59),
+            },
+        },
+    )
 
 
 @router.get("/creator/{account_id}", response_class=HTMLResponse)
-async def creator_details_page(request: Request, account_uow: AccountUoWDepend, task_adapter: ReportTaskAdapterDepend, task_uow: TaskUoWDepend, account_id: UUID, dto: CreatorVideosListParamsDTO = Depends()):
+async def creator_details_page(
+    request: Request,
+    account_uow: AccountUoWDepend,
+    task_adapter: ReportTaskAdapterDepend,
+    task_uow: TaskUoWDepend,
+    account_id: UUID,
+    dto: CreatorVideosListParamsDTO = Depends(),
+):
     account = await GetAccountUseCase(account_uow).execute(account_id)
     videos = await GetCreatorVideosUseCase(task_uow).execute(account_id, dto)
     from_datetime = datetime.datetime.now()
-    report = await MakeReportUseCase(task_adapter).execute(account_id, ReportMakeDTO(from_datetime=from_datetime, to_datetime=datetime.datetime.now() - datetime.timedelta(days=from_datetime.weekday())))
-    return templates.TemplateResponse("creator.html", {"request": request, "account": account, "videos": videos, "report": report})
+    report = await MakeReportUseCase(task_adapter).execute(
+        account_id,
+        ReportMakeDTO(
+            from_datetime=from_datetime,
+            to_datetime=datetime.datetime.now() - datetime.timedelta(days=from_datetime.weekday()),
+        ),
+    )
+    return templates.TemplateResponse(
+        "creator.html", {"request": request, "account": account, "videos": videos, "report": report}
+    )
 
 
 @router.get("/create", response_class=HTMLResponse)
@@ -47,10 +83,14 @@ async def creator_create_page(request: Request):
 
 
 @router.post("/create", response_class=HTMLResponse)
-async def create_creator(account_uow: AccountUoWDepend, task_uow: TaskUoWDepend,
-        http_client: HttpClientDepend,
-        account_adapter: AccountAdapterDepend,
-        runner: TaskRunnerDepend, dto: CreatorCreateDTO = Depends(CreatorCreateDTO.as_form)):
+async def create_creator(
+    account_uow: AccountUoWDepend,
+    task_uow: TaskUoWDepend,
+    http_client: HttpClientDepend,
+    account_adapter: AccountAdapterDepend,
+    runner: TaskRunnerDepend,
+    dto: CreatorCreateDTO = Depends(CreatorCreateDTO.as_form),
+):
     account = await CreateCreatorUseCase(account_uow).execute(dto)
     await RunCreatorScrapUseCase(task_uow, runner, account_adapter, http_client).execute(account)
     return RedirectResponse(url="/", status_code=303)
--- backend/src/web/domain/dtos.py
+++ backend/src/web/domain/dtos.py
@@ -25,8 +25,21 @@ class CreatorCreateDTO(BaseModel):
     fotobudka_url: str | None = None
 
     @classmethod
-    def as_form(cls, name: str = Form(), youtube_username: str | None = Form(None), tiktok_username: str | None = Form(None), instagram_username: str | None = Form(None), fotobudka_url: str | None = Form(None)):
-        return cls(name=name, youtube_username=youtube_username, tiktok_username=tiktok_username, instagram_username=instagram_username, fotobudka_url=fotobudka_url)
+    def as_form(
+        cls,
+        name: str = Form(),
+        youtube_username: str | None = Form(None),
+        tiktok_username: str | None = Form(None),
+        instagram_username: str | None = Form(None),
+        fotobudka_url: str | None = Form(None),
+    ):
+        return cls(
+            name=name,
+            youtube_username=youtube_username,
+            tiktok_username=tiktok_username,
+            instagram_username=instagram_username,
+            fotobudka_url=fotobudka_url,
+        )
 
 
 class CreatorVideosListParamsDTO(BaseModel):
